115580bfe87bf3ca70f448a848e72d57b4f0653e
Jing Li <42102899+lijing1996@users.noreply.github.com>
2022-08-29 17:51:18 +0800

    Update

--- images/resize_img.py
+++ images/resize_img.py
@@ -2,11 +2,16 @@ from PIL import Image, ImageOps
 import os
 images = os.listdir()
 for image in images:
-    if ".png" not in image:
+    if ".png" not in image or "256" in image:
         continue
     c = Image.open(image)
     w, h = c.size
-    pad = (256 - int(h / w * 256)) // 2
-    c = c.resize((256, int(h / w * 256)))
-    c = ImageOps.expand(c, border=(0, pad, 0, pad))
+    if w >= h:
+        pad = (256 - int(h / w * 256)) // 2
+        c = c.resize((256, int(h / w * 256)))
+        c = ImageOps.expand(c, border=(0, pad, 0, pad))
+    else:
+        pad = (256 - int(w / h * 256)) // 2
+        c = c.resize((int(w / h * 256), 256))
+        c = ImageOps.expand(c, border=(pad, 0, pad, 0))
     c.save("256_" + image)
